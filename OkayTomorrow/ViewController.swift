cc95be62027739c0e563ab2a7dbbae42c9261979
Alexander Mescheryakov <sibsfinx@ya.ru>
2014-10-31 21:48:06 +0300

    Initial Commit

---
+++ OkayTomorrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OkayTomorrow
+//
+//  Created by Alexander Mescheryakov on 31.10.14.
+//  Copyright (c) 2014 Alexander Mescheryakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
